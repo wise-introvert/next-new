e3915967fe74a88b52b68f480c1b3d212658609c
Fardeen Panjwani <fardeen.panjwani@ucmouli.com>
2020-10-22 11:34:54 +0530

    fix(_app): fixed type error
    Fixed "Type error: Binding element 'Component' implicitly has an 'any' type." error.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+const MyApp: React.FunctionComponent<any> = ({ Component, pageProps }: any): React.ReactElement => {
   return <Component {...pageProps} />
 }
 
